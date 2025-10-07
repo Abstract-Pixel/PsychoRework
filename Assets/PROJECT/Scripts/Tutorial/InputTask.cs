5d19b65dbf28b3255c6d6ecf3318ddff7cae5cb6
Armaan Takyar <armaantakyar@gmail.com>
2025-10-07 14:10:13 +0400

    EVERYTHING ELSE

---
+++ Assets/PROJECT/Scripts/Tutorial/InputTask.cs
@@ -0,0 +1,46 @@
+﻿using CustomInspector;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputTask : TutorialTask
+{
+    [SerializeField] 
+    private InputActionReference[] requiredActions;
+    
+    [SerializeField] bool requireAllActions;
+    [SerializeField][ShowIf(nameof(requireAllActions))] bool simultaneousActions = false;
+    [Resettable] bool cancelled = false;
+
+    
+    protected override void Start()
+    {
+        base.Start();
+        foreach (InputActionReference action in requiredActions)
+        {
+            action.action.Enable();
+        }
+    }
+
+    private void Update()
+    {
+        if (cancelled || !started) { return; }
+        foreach (InputActionReference action in requiredActions)
+        {
+            action.action.performed += CompleteAction;
+        }
+    }
+
+    private void CompleteAction(InputAction.CallbackContext context)
+    {
+        Debug.Log($"{context.action} performed");
+        CompleteTask();
+        cancelled = true;    
+    }
+
+    public override void CancelTask()
+    {
+        cancelled = true;
+    }
+    
+}
